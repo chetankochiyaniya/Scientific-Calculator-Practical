e0f7e1586a3b64bd6cfc7f157da46bb04b614776
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-01-30 17:13:17 +0530

    complete basic calculator design with basic functions (add,sub,mul,div,back,clear)

---
+++ index.js
@@ -0,0 +1,13 @@
+function elem(val){
+    display.value+=val;
+}
+
+function equal()
+{
+    display.value = eval( display.value)
+}
+
+function clear_val(){
+    console.log("cccc")
+    display.value = " ";
+}
